0c1d4147a704f9375a30f874be4e817752e2a408
BidyanshIndian <71256652+BidyanshIndian@users.noreply.github.com>
2023-05-17 16:55:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+song1="";
+song2="";
+leftwristX=0;
+rightwristX=0;
+leftwristY=0;
+rightwristY=0;
+function setup(){
+    canvas=createCanvas(600,500);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
+    posenet=ml5.poseNet(video,modelloaded);
+    posenet.on('pose',gotPoses);
+}
+function draw(){
+    image(video,0,0,600,500);
+}
+function preload(){
+    song1=loadSound("music.mp3");
+    song2=loadSound("Vegedream_-_Ramenez_La_Coupe_La_Maison-jukeboxmusic.com.ng.mp3");
+}
+function modelloaded(){
+    console.log("posenet is initialized");
+}
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+        leftwristX=results[0].pose.leftWrist.x;
+        leftwristY=results[0].pose.leftWrist.y;
+        console.log("leftwristX = "+ leftwristX +" leftwristY = "+leftwristY);
+        rightwristX=results[0].pose.rightWrist.x;
+        rightwristY=results[0].pose.rightWrist.y;
+        console.log("rightwristX = "+ rightwristX +" rightwristY = "+rightwristY);
+    }
+}
